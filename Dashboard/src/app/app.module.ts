7a47715848218172a6b6c62ec352533dbc6da998
Tarik Daouine <tarikpro19@gmail.com>
2022-09-20 10:47:08 +0200

    Initialisation Projet

---
+++ Dashboard/src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { NavBarLeftComponent } from './nav-bar-left/nav-bar-left.component';
+import { PasswordLostComponent } from './password-lost/password-lost.component';
+import { SearchBarComponent } from './search-bar/search-bar.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    LoginComponent,
+    RegisterComponent,
+    NavBarComponent,
+    NavBarLeftComponent,
+    PasswordLostComponent,
+    SearchBarComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
